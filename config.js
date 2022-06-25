d4c51929245ff62745f2d452b722a2e7802a7d5d
ashwinh7706 <77853233+ashwinh7706@users.noreply.github.com>
2022-06-24 21:50:30 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCNjuB4deOPPxTYePfFrki6mFkrORbaup8",
+  authDomain: "storytellingapp-ad2a8.firebaseapp.com",
+  projectId: "storytellingapp-ad2a8",
+  storageBucket: "storytellingapp-ad2a8.appspot.com",
+  messagingSenderId: "583937572089",
+  appId: "1:583937572089:web:5dd6e8bdef471c60783628"
+};
